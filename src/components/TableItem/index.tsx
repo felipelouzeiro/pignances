612e93ae768f15cc84c8e1beed244244025f903a
felipe <fl.louzeiro@gmail.com>
2022-08-24 15:45:47 -0300

    ✨ feat: implementando exibição das categorias

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -1,3 +1,4 @@
+import { categories } from '../../data/categories';
 import { formatDate } from '../../services/helpers/dateFilter';
 import { Item } from '../../types';
 import * as C from './styles';
@@ -10,7 +11,7 @@ export const TableItem = ({ item }: TableItemProps) => {
   return (
     <C.TableLine>
       <C.TableColumn>{formatDate(item.date)}</C.TableColumn>
-      <C.TableColumn>{item.category}</C.TableColumn>
+      <C.TableColumn>{categories[item.category].title}</C.TableColumn>
       <C.TableColumn>{item.title}</C.TableColumn>
       <C.TableColumn>R$ {item.value}</C.TableColumn>
     </C.TableLine>
--- src/data/categories.ts
+++ src/data/categories.ts
@@ -3,6 +3,6 @@ import { Category } from '../types';
 export const categories: Category = {
   food: { title: 'Alimentação', color: 'orange', expense: true },
   rent: { title: 'Aluguel', color: 'brown', expense: true },
-  leisure: { title: 'Diversão', color: 'pink', expense: true },
+  leisure: { title: 'Lazer', color: 'pink', expense: true },
   salary: { title: 'Alimentação', color: 'green', expense: false },
 };
