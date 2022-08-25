92162c8bdad179d251865b3a54880c253f813655
felipe <fl.louzeiro@gmail.com>
2022-08-25 00:18:28 -0300

    💄  feat: alterações no estilo/2

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -1,18 +1,35 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+`;
 
 export const Header = styled.div`
   background-color: #7cb490;
-  height: 150px;
+  height: 9rem;
   text-align: center;
+  text-shadow: 1px 1px #fff2af;
+
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+
+  svg {
+    font-size: 3rem;
+    margin-top: 30px;
+    color: #fff2af;
+  }
 `;
 
 export const HeaderTitle = styled.h1`
-  margin: 0;
+  margin-top: 30px;
+  font-size: 2.5rem;
   padding: 0;
-  color: #fff;
-  padding-top: 30px;
+  color: #7cb490;
+  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: #fff;
 `;
 
 export const Body = styled.div`
